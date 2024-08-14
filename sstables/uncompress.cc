1390e8ee4e3fbd475f806d63534089b6e24b67f1
Nikos Dragazis <nikolaos.dragazis@scylladb.com>
2024-08-14 19:48:49 +0300

    sstables: Remove redundant reader permit object
    For uncompressed SSTables, the data source implementation does not
    allocate any extra memory, so there is no need for a permit.
    
    Refs #19058
    
    Signed-off-by: Nikos Dragazis <nikolaos.dragazis@scylladb.com>

--- sstables/uncompress.cc
+++ sstables/uncompress.cc
@@ -11,7 +11,6 @@
 
 #include "seastar/core/future.hh"
 #include "types.hh"
-#include "reader_permit.hh"
 #include "seastar/core/temporary_buffer.hh"
 
 namespace sstables {
@@ -19,7 +18,6 @@ namespace sstables {
 class uncompressed_file_data_source_impl : public data_source_impl {
     std::optional<input_stream<char>> _input_stream;
     checksum& _checksum;
-    reader_permit _permit;
     uint64_t _underlying_pos;
     uint64_t _pos;
     //FIXME: Do we actually need this data member?
@@ -27,11 +25,9 @@ class uncompressed_file_data_source_impl : public data_source_impl {
     uint64_t _end_pos;
 public:
     uncompressed_file_data_source_impl(file f, checksum& checksum,
-    uint64_t pos, size_t len, file_input_stream_options options,
-    reader_permit permit)
+    uint64_t pos, size_t len, file_input_stream_options options)
     : _input_stream(make_file_input_stream(std::move(f), pos, len, std::move(options)))
     , _checksum(checksum)
-    , _permit(std::move(permit))
     , _underlying_pos(pos)
     , _pos(pos)
     , _beg_pos(pos)
@@ -44,10 +40,9 @@ public:
         }
         //FIXME:
         //What if the _pos is not aligned at the chunk size?
-        //What about the permit? How should I update it?
         return _input_stream->read_exactly(_checksum.chunk_size).then([this](temporary_buffer<char> buf) {
             _pos += buf.size();
-            return make_tracked_temporary_buffer(std::move(buf), std::move(res_units));
+            return buf;
         });
     }
 
@@ -74,18 +69,18 @@ public:
 class uncompressed_file_data_source : public data_source {
 public:
     uncompressed_file_data_source(file f, checksum& checksum, uint64_t offset,
-    size_t len, file_input_stream_options options, reader_permit permit)
+    size_t len, file_input_stream_options options)
     : data_source(std::make_unique<uncompressed_file_data_source_impl>(
-        std::move(f), checksum, offset, len, std::move(options), std::move(permit)))
+        std::move(f), checksum, offset, len, std::move(options)))
     {}
 };
 
 inline input_stream<char> make_uncompressed_file_input_stream(
     file f, checksum& checksum, uint64_t offset, size_t len,
-    file_input_stream_options options, reader_permit permit)
+    file_input_stream_options options)
 {
     return input_stream<char>(uncompressed_file_data_source(
-        std::move(f), checksum, offset, len, std::move(options), std::move(permit)));
+        std::move(f), checksum, offset, len, std::move(options)));
 }
 
 }
