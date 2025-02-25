54a0f1c974f9ee23c8160ff282f390187941292e
Nikos Dragazis <nikolaos.dragazis@scylladb.com>
2025-02-25 16:25:09 +0200

    encryption: Add skeleton for Azure host
    The Azure host manages cryptographic keys using Azure Key Vault.
    
    This patch only defines the API.
    
    Signed-off-by: Nikos Dragazis <nikolaos.dragazis@scylladb.com>

---
+++ ent/encryption/azure_host.cc
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2025 ScyllaDB
+ *
+ */
+
+/*
+ * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
+ */
+
+#include <stdexcept>
+
+#include "azure_host.hh"
+
+namespace encryption {
+
+class azure_host::impl {
+public:
+    impl(const std::string& name, const host_options&);
+};
+
+azure_host::impl::impl(const std::string& name, const host_options&) {}
+
+// ==================== azure_host class implementation ====================
+
+azure_host::azure_host(const std::string& name, const host_options& options) : _impl(std::make_unique<impl>(name, options)) {}
+
+azure_host::~azure_host() = default;
+
+future<> azure_host::init() {
+    throw std::logic_error("Not implemented");
+}
+
+future<azure_host::key_and_id_type> azure_host::get_or_create_key(const key_info& info) {
+    throw std::logic_error("Not implemented");
+}
+
+future<azure_host::key_ptr> azure_host::get_key_by_id(const azure_host::id_type& id, const key_info& info) {
+    throw std::logic_error("Not implemented");
+}
+
+}
---
+++ ent/encryption/azure_host.hh
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2025 ScyllaDB
+ *
+ */
+
+/*
+ * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
+ */
+
+#pragma once
+
+#include <memory>
+
+#include <seastar/core/future.hh>
+#include <seastar/core/shared_ptr.hh>
+
+#include "symmetric_key.hh"
+
+namespace encryption {
+
+class azure_host {
+    class impl;
+    std::unique_ptr<impl> _impl;
+public:
+    using id_type = bytes;
+    using key_ptr = shared_ptr<symmetric_key>;
+    using key_and_id_type = std::tuple<key_ptr, id_type>;
+
+    struct host_options {
+        std::string tenant_id;
+        std::string client_id;
+        std::string client_secret;
+        std::string client_cert;
+
+        // Azure Key Vault Key to encrypt data keys with. Format: <vault name>/<keyname>
+        std::string master_key;
+
+        // TLS options
+        std::string truststore;
+        std::string priority_string;
+
+        std::optional<std::chrono::milliseconds> key_cache_expiry;
+        std::optional<std::chrono::milliseconds> key_cache_refresh;
+    };
+
+    azure_host(const std::string& name, const host_options&);
+    ~azure_host();
+
+    future<> init();
+
+    /**
+     * Key Management API to create or retrieve data keys.
+     *
+     * Throws encryption::base_error derivatives to indicate failure conditions
+     * such as host misconfigurations, network issues, or authentication failures.
+     */
+    future<key_and_id_type> get_or_create_key(const key_info&);
+    future<key_ptr> get_key_by_id(const id_type&, const key_info&);
+};
+
+}
