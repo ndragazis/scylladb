6dc395363be72e45593c10ed6e578d1ccaad1f18
Nikos Dragazis <nikolaos.dragazis@scylladb.com>
2025-03-19 12:15:34 +0200

    encryption: Add skeleton for azure host impl
    Signed-off-by: Nikos Dragazis <nikolaos.dragazis@scylladb.com>

--- ent/encryption/azure_host.cc
+++ ent/encryption/azure_host.cc
@@ -15,25 +15,42 @@ namespace encryption {
 
 class azure_host::impl {
 public:
-    impl(const host_options&);
+    impl(const host_options& options);
+    future<> init();
+    future<key_and_id_type> get_or_create_key(const key_info&);
+    future<key_ptr> get_key_by_id(const id_type&, const key_info&);
+private:
+    const host_options _options;
 };
 
 azure_host::impl::impl(const host_options&) {}
+
+future<> azure_host::impl::init() {
+    throw std::logic_error("Not implemented");
+}
+
+future<azure_host::key_and_id_type> azure_host::impl::get_or_create_key(const key_info& info) {
+    throw std::logic_error("Not implemented");
+}
+
+future<azure_host::key_ptr> azure_host::impl::get_key_by_id(const azure_host::id_type& id, const key_info& info) {
+    throw std::logic_error("Not implemented");
+}
 
 // ==================== azure_host class implementation ====================
 
 azure_host::azure_host(const host_options& options) : _impl(std::make_unique<impl>(options)) {}
 
 future<> azure_host::init() {
-    throw std::logic_error("Not implemented");
+    return _impl->init();
 }
 
 future<azure_host::key_and_id_type> azure_host::get_or_create_key(const key_info& info) {
-    throw std::logic_error("Not implemented");
+    return _impl->get_or_create_key(info);
 }
 
 future<azure_host::key_ptr> azure_host::get_key_by_id(const azure_host::id_type& id, const key_info& info) {
-    throw std::logic_error("Not implemented");
+    return _impl->get_key_by_id(id, info);
 }
 
 }
