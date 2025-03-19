533f533677ce496b99eb940f190eebb88d51cdf8
Nikos Dragazis <nikolaos.dragazis@scylladb.com>
2025-03-19 19:59:11 +0200

    encryption: azure: Implement get_or_create_key()
    Implement the `azure_host::get_or_create_key()` API, which returns a
    data encryption key for a given algorithm descriptor (cipher algorithm
    and key length).
    
    Use a loading cache to reduce the API calls to Key Vault. When the cache
    needs to refresh or reload a key, always create a new one and wrap it
    with the Vault key. Use the encryption::httpclient with
    'tls_wait_on_close=false' to connect to Key Vault (the service does not
    respond to TLS close_notify alerts).
    
    Use the following identifier for each key:
    <vault name>/<key name>/<key version>:<base64 encoded ciphertext of data encryption key>
    
    The key version is required to support Vault key rotations.
    
    Finally, define an exception for Vault errors. The exception contains
    the error code and error message from the response payload.
    
    Signed-off-by: Nikos Dragazis <nikolaos.dragazis@scylladb.com>

--- ent/encryption/azure_host.hh
+++ ent/encryption/azure_host.hh
@@ -20,6 +20,27 @@ namespace encryption {
 
 class encryption_context;
 
+class vault_error : public std::exception {
+    // The error response always contains a code and a message.
+    // https://github.com/microsoft/api-guidelines/blob/vNext/azure/Guidelines.md#handling-errors
+    static constexpr char ERROR_KEY[] = "error";
+    static constexpr char ERROR_CODE_KEY[] = "code";
+    static constexpr char ERROR_MESSAGE_KEY[] = "message";
+    std::string _code, _msg;
+public:
+    vault_error(std::string_view code, std::string_view msg)
+        : _code(code)
+        , _msg(fmt::format("{}: {}", code, msg))
+    {}
+    const std::string& code() const {
+        return _code;
+    }
+    const char* what() const noexcept override {
+        return _msg.c_str();
+    }
+    static vault_error make_error(std::string_view result);
+};
+
 class azure_host {
 public:
     class impl;
