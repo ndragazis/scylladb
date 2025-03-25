db852077849bfbd3c0e9072686baef31abd62c1e
Nikos Dragazis <nikolaos.dragazis@scylladb.com>
2025-03-25 20:28:24 +0200

    encryption: Call override options from Azure Key Provider
    Signed-off-by: Nikos Dragazis <nikolaos.dragazis@scylladb.com>

--- ent/encryption/azure_key_provider.cc
+++ ent/encryption/azure_key_provider.cc
@@ -14,9 +14,10 @@ namespace encryption {
 
 class azure_key_provider : public key_provider {
 public:
-    azure_key_provider(::shared_ptr<azure_host> azure_host, std::string name)
+    azure_key_provider(::shared_ptr<azure_host> azure_host, std::string name, azure_host::option_override oov)
         : _azure_host(std::move(azure_host))
         , _name(std::move(name))
+        , _oov(std::move(oov))
     {}
     future<std::tuple<key_ptr, opt_bytes>> key(const key_info& info, opt_bytes id) override {
         if (id) {
@@ -24,7 +25,7 @@ public:
                 return make_ready_future<std::tuple<key_ptr, opt_bytes>>(std::tuple(k, id));
             });
         }
-        return _azure_host->get_or_create_key(info).then([](std::tuple<key_ptr, opt_bytes> k_id) {
+        return _azure_host->get_or_create_key(info, &_oov).then([](std::tuple<key_ptr, opt_bytes> k_id) {
             return make_ready_future<std::tuple<key_ptr, opt_bytes>>(k_id);
         });
     }
@@ -34,6 +35,7 @@ public:
 private:
     ::shared_ptr<azure_host> _azure_host;
     std::string _name;
+    azure_host::option_override _oov;
 };
 
 shared_ptr<key_provider> azure_key_provider_factory::get_provider(encryption_context& ctxt, const options& map) {
@@ -45,13 +47,17 @@ shared_ptr<key_provider> azure_key_provider_factory::get_provider(encryption_con
         throw std::invalid_argument("azure_host must be provided");
     }
 
+    azure_host::option_override oov {
+        .master_key =  opts("master_key"),
+    };
+
     auto host = ctxt.get_azure_host(*azure_host);
-    auto id = azure_host.value();
+    auto id = azure_host.value() + ":" + oov.master_key.value_or(host->options().master_key);
 
     auto provider = ctxt.get_cached_provider(id);
 
     if (!provider) {
-        provider = ::make_shared<azure_key_provider>(host, *azure_host);
+        provider = ::make_shared<azure_key_provider>(host, *azure_host, std::move(oov));
         ctxt.cache_provider(id, provider);
     }
 
