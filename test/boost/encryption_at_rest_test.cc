775170bedacb7cde2c80713b62e1843516cbae94
Nikos Dragazis <nikolaos.dragazis@scylladb.com>
2025-07-09 19:56:02 +0300

    test: Making KMIP tests fail to preserve logs and tmpdir
    Signed-off-by: Nikos Dragazis <nikolaos.dragazis@scylladb.com>

--- test/boost/encryption_at_rest_test.cc
+++ test/boost/encryption_at_rest_test.cc
@@ -448,6 +448,8 @@ database_path={}/pykmip.db
     }
 
     co_await f(info, tmp);
+
+    throw std::runtime_error("Deliberate exception to preserve the tmpdir.");
 }
 
 SEASTAR_TEST_CASE(test_kmip_provider, *check_run_test_decorator("ENABLE_KMIP_TEST", true)) {
