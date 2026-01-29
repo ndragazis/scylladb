29d4d8893006b854b3fd9f21578a9436ce091ade
Nikos Dragazis <nikolaos.dragazis@scylladb.com>
2026-01-29 18:30:57 +0200

    Revert "table_helper: Use prepare_options for auto-expansion (take 3)"
    This reverts commit a2c7e78f670970a7e5f66a06a533a66cfb3c528e.

--- table_helper.cc
+++ table_helper.cc
@@ -8,14 +8,11 @@
  */
 
 #include "cql3/statements/property_definitions.hh"
-#include "db/config.hh"
-#include "gms/feature_service.hh"
 #include "utils/assert.hh"
 #include <seastar/core/coroutine.hh>
 #include <seastar/coroutine/parallel_for_each.hh>
 #include "table_helper.hh"
 #include "cql3/query_processor.hh"
-#include "cql3/statements/ks_prop_defs.hh"
 #include "cql3/statements/create_table_statement.hh"
 #include "cql3/statements/modification_statement.hh"
 #include "replica/database.hh"
@@ -172,12 +169,14 @@ future<> table_helper::setup_keyspace(cql3::query_processor& qp, service::migrat
         auto ts = group0_guard.write_timestamp();
 
         if (!db.has_keyspace(keyspace_name)) {
-            auto tm = qp.proxy().get_token_metadata_ptr();
-            const auto& cfg = db.real_database().get_config();
-            bool rack_list_enabled = db.real_database().features().rack_list_rf;
             locator::replication_strategy_config_options opts;
-            opts["replication_factor"] = replication_factor;
-            opts = cql3::statements::ks_prop_defs::prepare_options(replication_strategy_name, *tm, cfg.rf_rack_valid_keyspaces(), cfg.enforce_rack_list(), std::move(opts), {}, rack_list_enabled, initial_tablets.has_value());
+            if (replication_strategy_name == "org.apache.cassandra.locator.NetworkTopologyStrategy") {
+                for (const auto &dc: qp.proxy().get_token_metadata_ptr()->get_topology().get_datacenters())
+                    opts[dc] = replication_factor;
+            }
+            else {
+                opts["replication_factor"] = replication_factor;
+            }
             auto ksm = keyspace_metadata::new_keyspace(keyspace_name, replication_strategy_name, std::move(opts), initial_tablets, std::nullopt, true);
             try {
                 co_await mm.announce(service::prepare_new_keyspace_announcement(db.real_database(), ksm, ts),
