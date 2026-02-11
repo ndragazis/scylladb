979b67a5353161621b114f53549f35669283c5d7
Nikos Dragazis <nikolaos.dragazis@scylladb.com>
2026-02-11 11:54:24 +0200

    TMP: Disable retries on DDL statements
    Signed-off-by: Nikos Dragazis <nikolaos.dragazis@scylladb.com>

--- cql3/query_processor.cc
+++ cql3/query_processor.cc
@@ -554,7 +554,8 @@ future<::shared_ptr<cql_transport::messages::result_message>> query_processor::e
     }
 
     auto [remote_, holder] = remote();
-    size_t retries = remote_.get().mm.get_concurrent_ddl_retries();
+    //size_t retries = remote_.get().mm.get_concurrent_ddl_retries();
+    size_t retries = 0;
     while (true)  {
         try {
             auto guard = co_await remote_.get().mm.start_group0_operation();
